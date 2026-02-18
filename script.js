3ad864b07111c9bb5004f3cf09dcd0539e3fbd15
LJDJGC <singsongsmusic@gmail.com>
2026-02-19 06:22:48 +0900

    feat: implement Gameboard module with private array and accessors

---
+++ script.js
@@ -0,0 +1,24 @@
+const Gameboard = (() => {
+    //3x3の盤面
+    let board = ["", "", "", "", "", "", "", "", "", ];
+
+    const getBoard = () => [...board];
+
+    const placeMarker = (index, marker) => {
+        if (board[index] === "") {
+            board[index] = marker;
+            return true;
+        }
+        return false;
+    };
+
+    return { getBoard, placeMarker};
+})();
+
+const Player = (name, marker) => {
+
+};
+
+const gameController = (() => {
+
+})();
