a8ebc892797a6c20d7438d61e6d8d4c34efe511c
LJDJGC <singsongsmusic@gmail.com>
2026-02-26 22:08:25 +0900

    feat: scaffold GameController and define winning logic

--- script.js
+++ script.js
@@ -20,7 +20,12 @@ const Player = (name, marker) => {
 };
 
 const gameController = (() => {
-    const players =
+    const players = [
+        Player("Player 1", "X"),
+        Player("Player 2", "0")
+    ];
+
+    let activePlayer = players[0];
 
 
 
@@ -29,4 +34,16 @@ const gameController = (() => {
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
     ];
+
+
+    const switchPlayerTurn = () => {
+        // TODO: activePlayer を切り替える（三項演算子がおすすめ）
+        // activePlayer = activePlayer === players[0] ? ... : ...;
+    };
+
+    const getActivePlayer = () => {
+        const board = Gameboard.getBoard();
+        console.log(`${getActivePlayer().name} のターンです。`);
+        console.log(board);
+    }
 })();
